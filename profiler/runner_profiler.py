33abd05ba9f7af94e0f08cede05dd50a074c3619
Lucas Nestor <lucas15nestor@gmail.com>
2020-11-05 12:18:13 -0500

    Move correctness checker to own directory for now

--- profiler/runner_profiler.py
+++ profiler/runner_profiler.py
@@ -4,12 +4,12 @@ def read_formula():
     with open("profiler/formula.txt") as f:
         return f.read()
 
-def profile_naive_runner():
+def profile_algorithm1_runner():
     formula = read_formula()
 
     SETUP_CODE = """
 from context import sat_solver
-from sat_solver.naive_runner import NaiveRunner
+from sat_solver.algorithm1_runner import Algorithm1Runner
 from sat_solver.bool import Bool
 from sat_solver.literal import Literal
 from sat_solver.clause import Clause
@@ -17,18 +17,18 @@ from sat_solver.clause import Clause
 
 
     TEST_CODE = """
-runner = NaiveRunner(clause_list, atom_list)
+runner = Algorithm1Runner(clause_list, atom_list)
 runner.run()"""
 
     times = timeit.timeit(setup = SETUP_CODE, stmt = TEST_CODE, number = 1000)
     print(times)
 
-def profile_pruning_runner():
+def profile_algorithm2_runner():
     formula = read_formula()
 
     SETUP_CODE = """
 from context import sat_solver
-from sat_solver.pruning_runner import PruningRunner
+from sat_solver.algorithm2_runner import Algorithm2Runner
 from sat_solver.bool import Bool
 from sat_solver.literal import Literal
 from sat_solver.clause import Clause
@@ -36,12 +36,12 @@ from sat_solver.clause import Clause
 
 
     TEST_CODE = """
-runner = PruningRunner(clause_list, atom_list)
+runner = Algorithm2Runner(clause_list, atom_list)
 runner.run()"""
 
     time = timeit.timeit(setup = SETUP_CODE, stmt = TEST_CODE, number = 1000)
     print(time)
 
 if __name__ == "__main__":
-    profile_naive_runner()
-    profile_pruning_runner()
+    profile_algorithm1_runner()
+    profile_algorithm2_runner()
