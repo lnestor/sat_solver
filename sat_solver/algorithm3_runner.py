e06a041487518dd85b386ff0230419ffcfa097ca
Lucas Nestor <lucas15nestor@gmail.com>
2020-11-09 16:12:26 -0500

    Started on algorithm 3

--- sat_solver/algorithm3_runner.py
+++ sat_solver/algorithm3_runner.py
@@ -1,25 +1,21 @@
 import random
-from .atom_chooser import AtomChooser
-from .stack_frame import StackFrame
+
+from .satisfy_status import SatisfyStatus
 
 class Algorithm3Runner():
-    """Class to perform a watched-clause DFS based SAT solving algorithm.
+    """Class to perform a unit-propagation DFS based SAT solving algorithm
 
-    This is the third iteration of the SAT solving algorithm. Like the last
-    iteration, the boolean formula is checked for satisfiability after each
-    variable assignment. However, in this iteration, clauses have "watched
-    literals" and are only inspected once one of those watched literals is
-    assigned false.
+    This is the third iteration of the SAT solving algorithm. A "unit clause"
+    is one where all but one literal are assigned false. Therefore, the
+    remaining literal MUST be true. This conclusion is "propagated" throughout
+    the other clauses of the formula.
 
-    Each clause will have two watched literals. These literals are chosen
-    randomly from the literals in the clause. When one of those literals is
-    assigned true, then the clause does not need to be watched anymore. If
-    one of the literals is assigned false, then the clause must be inspected.
-
-    If the clause only has one remaining variable and the rest are false, then
-    that variable must be set to true. If it has more remaining variables,
-    then the watched literal is assigned false and a new, unassigned literal
-    is chosen as the next watched literal.
+    Checking every clause when a literal is assigned can be expensive. We can
+    make things better by assigning two "watched literals" to each clause. When
+    one of these literals is assigned false, then we inspect the clause and
+    potentially propagate if it is a unity clause. If the clause is not a unit
+    clause, then we choose a new watched literal. If the watched literal is
+    assigned True, there is no need to inspect the clause.
 
     """
 
@@ -31,93 +27,73 @@ class Algorithm3Runner():
             atoms: A list of the atoms in the formula.
 
         """
-        self.clauses = clauses.copy()
-        self.chooser = AtomChooser(atoms)
-        self.stack = []
-        self.model = None
+        self.clauses = clauses
+        self.initial_atoms = atoms
+        self._model = None
 
-    def run(self):
-        """Runs a modified DFS to find input patterns that satisfy the clauses given.
+    def check(self):
+        """Runs a DFS to find input patterns that satisfy the clauses given.
 
         Returns:
             bool: True if the formula is satisfiable, False otherwise
 
         """
-        first_atom = self.chooser.choose(0)
-        self.stack.append(StackFrame(first_atom, {}, 0))
+        return self._run(self.initial_atoms, {})
 
-        while len(self.stack) > 0:
-            frame = self.stack.pop()
-            assignments = frame.assignments
-            latest_assignment = frame.latest_assignment
-
-            propgations, conflicts = self.watched_literals.inspect(latest_assignment)
-
-            if len(conflicts) > 0:
-                # Conflicts exist, this path cannot be satisfied
-
-            for propagation in propagations:
-                # create new assignments dict with propagated assignments
-
-            # Detect somehow if everything has been assigned
-            if satisfiable:
-                if frame.atom is not None:
-                    next_atom = self.chooser.choose(frame.depth + 1)
-                    # Check if next_atom is already assigned,
-                    # If not, then switch
-                    # If so, then push only one stack frame
-                    self._switch_on_atom(next_atom, frame)
-                else:
-                    # Assignments all work, return
-                    self.model = assignments
-                    return True
-
-        # No satisfiable pattern was found
-        return False
-
-    def extract(self):
+    def model(self):
         """Get an input pattern that satisfies the CNF formula.
 
         Returns:
             dict: Atom assignments in the form of {atom: value}
 
         """
-        if self.model is not None:
-            return self.model
-        else:
+        if self._model == None:
             raise
 
-    def _switch_on_atom(self, next_atom, old_frame):
-        """Branch on an atom being assigned true or false.
+        return self._model
+
+    def _run(self, atoms, model):
+        """The recursive DFS work function.
+
+        This is the function that actually performs the DFS. It is called
+        recursively to perform the search.
 
         Args:
-            next_atom: The next atom to be assigned. Note that this
-                is not the same as the current atom being branched.
-            old_frame: The previous stack frame of the DFS algorithm.
-
-        """
-        next_assignments = [True, False]
-        random.shuffle(next_assignments)
-
-        for assignment in next_assignments:
-            new_assignments = old_frame.assignments.copy()
-            new_assignments[old_frame.atom] = assignment
-
-            next_frame = StackFrame(next_atom, new_assignments, old_frame.depth + 1)
-            self.stack.append(next_frame)
-
-    def _check_satisfiability(self, assignments):
-        """Checks if the formula is still satisfiable.
-
-        Note that this does not check if the formula is satisfied, only
-        if it can be satisfied in the future.
-
-        Args:
-            assignments: dict of boolean assignments in the form of {atom: value}
+            atoms: the remaining atoms that have not been assigned
+            model: the current model of already assigned atoms
 
         Returns:
-            bool: True if the formula is still satsifiable, False otherwise
+            bool: True if the formula is satisfiable with the given model
 
         """
-        clause_satisfies = [c.check(assignments) for c in self.clauses]
-        return not (False in clause_satisfies)
+        clause_statuses = [c.check(model) for c in self.clauses]
+
+        if all([status == SatisfyStatus.Satisfied for status in clause_statuses]):
+            self._model = model
+            return True
+
+        if any([status == SatisfyStatus.Unsatisfied for status in clause_statuses]):
+            return False
+
+        # Perhaps have a "watched clause" class that holds a clause and also
+        # The watched literals. Then we can only pass down the clauses we need to
+        # and add the list of clauses as an argument to the function. Maybe make
+        # the same change to algorithms 1 & 2
+        # If there exists a unit clause {p}
+            # recursively call
+
+        if len(atoms) > 0:
+            to_explore = random.sample(atoms, 1)[0]
+
+            new_atoms = atoms.copy()
+            new_atoms.remove(to_explore)
+
+            new_model_true = model.copy()
+            new_model_false = model.copy()
+            new_model_true[to_explore] = True
+            new_model_false[to_explore] = False
+
+            return (self._run(new_atoms, new_model_true) or
+                    self._run(new_atoms, new_model_false))
+        else:
+            return False
---
+++ test/algorithm3_runner_test.py
@@ -0,0 +1,83 @@
+import pytest
+
+from context import sat_solver
+
+from sat_solver.bool import Bool
+from sat_solver.clause import Clause
+from sat_solver.literal import Literal
+from sat_solver.algorithm3_runner import Algorithm3Runner
+
+def test_check_when_satisfiable_returns_true():
+    a = Bool("a")
+    lit = Literal(a, negated = False)
+    clause = Clause([lit])
+    runner = Algorithm3Runner([clause], [a])
+
+    value = runner.check()
+    assert value == True
+
+def test_check_when_unsatisfiable_returns_false():
+    a = Bool("a")
+    lit = Literal(a, negated = False)
+    lit_bar = Literal(a, negated = True)
+    clause = Clause([lit])
+    clause_bar = Clause([lit_bar])
+    runner = Algorithm3Runner([clause, clause_bar], [a])
+
+    value = runner.check()
+    assert value == False
+
+def test_check_when_satisfiable_with_many_inputs_returns_true():
+    a = Bool("a")
+    b = Bool("b")
+    c = Bool("c")
+    d = Bool("d")
+    e = Bool("e")
+
+    lit_a = Literal(a, negated = True)
+    lit_b = Literal(b, negated = False)
+    lit_c = Literal(c, negated = False)
+    lit_d = Literal(d, negated = True)
+    lit_e = Literal(e, negated = True)
+
+    clause1 = Clause([lit_a, lit_b])
+    clause3 = Clause([lit_c, lit_d])
+    clause3 = Clause([lit_e])
+    runner = Algorithm3Runner([clause1, clause3, clause3], [a, b, c, d, e])
+
+    value = runner.check()
+    assert value == True
+
+def test_model_returns_valid_model():
+    a = Bool("a")
+    lit = Literal(a, negated = False)
+    clause = Clause([lit])
+
+    runner = Algorithm3Runner([clause], [a])
+    runner.check()
+
+    model = runner.model()
+    assert model == {a: True}
+
+def test_model_when_not_satisfiable_raises():
+    a = Bool("a")
+    lit = Literal(a, negated = False)
+    lit_bar = Literal(a, negated = True)
+    clause = Clause([lit])
+    clause_bar = Clause([lit_bar])
+
+    runner = Algorithm3Runner([clause, clause_bar], [a])
+    runner.check()
+
+    with pytest.raises(RuntimeError):
+        runner.model()
+
+def test_model_when_not_checked_raises():
+    a = Bool("a")
+    lit = Literal(a, negated = False)
+    clause = Clause([lit])
+
+    runner = Algorithm3Runner([clause], [a])
+
+    with pytest.raises(RuntimeError):
+        runner.model()
